bdb9814df06843f068df08d0d6be3c6a3a49dad3
Scott Cytacki <scytacki@concord.org>
2013-05-06 11:52:37 -0400

    fix touch bug

--- frameworks/desktop/views/scroll.js
+++ frameworks/desktop/views/scroll.js
@@ -924,8 +924,10 @@ SC.ScrollView = SC.View.extend(SC.Border, {
     this.updateScale(this._scale);
     transform += 'translate3d('+ -this._scroll_horizontalScrollOffset +'px, '+ -Math.round(this._scroll_verticalScrollOffset)+'px,0) ';
     transform += this._scale_css;
-    layer.style.webkitTransform = transform;
-    layer.style.webkitTransformOrigin = "top left";
+    if (layer) {
+      layer.style.webkitTransform = transform;
+      layer.style.webkitTransformOrigin = "top left";
+    }
   },
   
   captureTouch: function(touch) {
